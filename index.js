7434dd32615b4e44c3aa004045a84f78afbfdf50
Pierce <pierceevans@me.com>
2022-12-16 14:55:14 -0600

    commit tree d41b6e9e49be6c6f073f8664c50ae9deb36b41ae

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(someWhere="roller-skate") {
+    return `This Saturday, I want to ${someWhere}!`;
+    }
+    saturdayFun()
+let mondayWork= function( willDo= "go to the office") {
+    return`This Monday, I will ${willDo}.`;
+}
+function wrapAdjective(flair="*") {
+    return function(string = "special") {
+        return `You are ${flair}${string}${flair}!`;
+    }
+}
